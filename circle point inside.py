b34cbe0d80c560b2161e4a020e3bf0f86c536fec
avanishmaurya <84256387+avanishmaurya@users.noreply.github.com>
2021-05-16 08:16:15 +0530

    point location with respect to circle

---
+++ circle point inside.py
@@ -0,0 +1,6 @@
+x=int(input("Enter x coordinate"))
+y=int(input("Enter y coordinate"))
+if (x**2+y**2)<100:
+    print("(",x,",",y,") is inside the circle")
+else:
+    print("coordinate is not inside the circle")
